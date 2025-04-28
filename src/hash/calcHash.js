40d7b75356488244bbf36d3724f462b107995c48
Komiljon Najmitdinov <komiljonnajmitdinov99@gmail.com>
2025-04-28 19:57:33 +0500

    feat: Implemented `hash` requirements

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
+import { createReadStream } from 'fs';
+import crypto from 'node:crypto';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const ERROR_MESSAGE = 'The sha256 hash calculation operation failed';
+const HASH_ALGORITHM = 'sha256';
+const FILE_NAME = 'fileToCalculateHashFor.txt';
+const FOLDER = 'files';
+const FOLDER_PATH = join(
+    fileURLToPath(import.meta.url),
+    '..',
+    FOLDER,
+    FILE_NAME
+);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = crypto.createHash(HASH_ALGORITHM);
+    const data = createReadStream(FOLDER_PATH);
+
+    data.on('error', (error) => {
+        throw new Error(`${ERROR_MESSAGE}: ${error}`);
+    });
+
+    data.pipe(hash)
+        .setEncoding('hex')
+        .on('finish', () => {
+            process.stdout.write(hash.read().toString('hex') + '\n');
+        });
 };
 
 await calculateHash();

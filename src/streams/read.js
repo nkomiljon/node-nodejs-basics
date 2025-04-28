b92dbee4401c44962328943ecf2e644345f38bd6
Komiljon Najmitdinov <komiljonnajmitdinov99@gmail.com>
2025-04-28 19:58:14 +0500

    Implemented `streams` requirements

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const ERROR_MESSAGE = '\nReadable stream operation failed';
+const FILE_NAME = 'fileToRead.txt';
+const FOLDER = 'files';
+const FOLDER_PATH = join(
+    fileURLToPath(import.meta.url),
+    '..',
+    FOLDER,
+    FILE_NAME
+);
+
 const read = async () => {
-    // Write your code here 
+    const readableStream = createReadStream(FOLDER_PATH);
+
+    readableStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readableStream.on('error', (error) => {
+        console.error(`${ERROR_MESSAGE}: ${error.message}`);
+    });
 };
 
 await read();
